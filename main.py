7bbd52783c4b06b260c94fe77bbad00b51f54002
yat1j <yatinarora156@gmail.com>
2026-05-27 22:47:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,3 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"status": "working"}
-
-@app.get("/test")
-def test():
-    try:
-        from src.crew import build_crew
-        return {"import": "success"}
-    except Exception as e:
-        return {"error": str(e)}
